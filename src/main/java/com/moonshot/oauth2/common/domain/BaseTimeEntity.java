a65d6a74d1084c0e400d0d2161938779c7c0be99
its-sky <smc9919@naver.com>
2023-12-21 00:26:17 +0900

    feat: BaseTimeEntity 생성, 수정 시간 기능

---
+++ src/main/java/com/moonshot/oauth2/common/domain/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.moonshot.oauth2.common.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
