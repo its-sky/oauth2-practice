25e4414d85392b434a9c1b60fee6783cd5a7f4b2
its-sky <smc9919@naver.com>
2023-12-21 00:26:36 +0900

    feat: 공통 ApiResponse 추가

---
+++ src/main/java/com/moonshot/oauth2/common/exception/enums/SuccessType.java
@@ -0,0 +1,33 @@
+package com.moonshot.oauth2.common.exception.enums;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessType {
+
+    /**
+     * 200 OK
+     */
+    DEFAULT_SUCCESS(HttpStatus.OK, "OK"),
+    SOCIAL_LOGIN_SUCCESS(HttpStatus.OK, "소셜 로그인 성공"),
+
+    /**
+     * 201 CREATED
+     */
+
+    /**
+     * 204 NO CONTENT
+     */
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+}
