5d5e442de6208a5099c594ff4775f7f4848ef416
its-sky <smc9919@naver.com>
2023-12-21 00:28:34 +0900

    feat: JpaAuditingConfig 설정

---
+++ src/main/java/com/moonshot/oauth2/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.moonshot.oauth2.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
